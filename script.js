8968021c16c73c6ce7eb6cf5c96b0cbb4767143f
MudassirAlam7 <mudassiralam7@gmail.com>
2024-12-19 23:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let codes = document.querySelectorAll('.code')
+let resetBtn = document.querySelector('#submit')
+codes.forEach((input, index) =>{
+    input.addEventListener('input', ()=>{
+        if(input.value && index<codes.length-1){
+            codes[index+1].focus()
+        }
+    })
+    input.addEventListener('keydown', (e)=>{
+        if(e.key === "Backspace" && !input.value && index>0){
+            codes[index-1].focus()
+        }
+    })
+})
